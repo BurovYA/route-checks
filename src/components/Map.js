0748412769a597545f0cdd05b91370d6d7dcb0f1
Yuri <burov.y.a@gmail.com>
2019-05-30 23:05:32 +0300

    Подложка карты "Спутник", Material компоненты

--- src/components/Map.js
+++ src/components/Map.js
@@ -1,10 +1,13 @@
 import React, { Component } from 'react';
 import styled from 'styled-components';
 
+import 'ol/ol.css';
 import OlMap from 'ol/Map';
 import { Tile as TileLayer } from 'ol/layer.js';
 import View from 'ol/View.js';
-import { OSM } from 'ol/source.js';
+import XYZ from 'ol/source/XYZ.js';
+import { ATTRIBUTION as OSM_ATTRIBUTION } from 'ol/source/OSM.js';
+import { defaults as defaultControls } from 'ol/control.js';
 
 const MapContainer = styled.div`
   height: 100%;
@@ -20,12 +23,21 @@ export default class Map extends Component {
   }
   componentDidMount() {
     const raster = new TileLayer({
-      source: new OSM()
+      source: new XYZ({
+        url: 'http://{a-c}.tilessputnik.ru/{z}/{x}/{y}.png',
+        opaque: false,
+        attributions: [
+          '<a href="https://maps.sputnik.ru">Спутник</a>',
+          '© Ростелеком',
+          OSM_ATTRIBUTION
+        ]
+      })
     });
 
     this.olMap = new OlMap({
       layers: [raster],
       target: this.mapContainer.current,
+      controls: defaultControls({ zoom: false }),
       view: new View({
         center: [0, 0],
         zoom: 2
